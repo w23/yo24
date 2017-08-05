d0560196bdf64df279a01e5096fab7eed2039fec
Ivan Avdeev <me@w23.ru>
2017-08-05 11:49:38 +0700

    fix thigs and optimize (3133)

--- header_fixed.h
+++ header_fixed.h
@@ -5,16 +5,16 @@
 # define HEADER_H_
 
 const char *header_glsl =
- "#version 130\n"
- "uniform sampler2D S[12];"
- "uniform float F[32];"
- //"vec3 f=vec3(F[0],F[1],F[2]),c=vec3(F[3],F[4],F[5]),e=vec3(0.,3.,0.),s=vec3(F[9],F[10],F[11]);"
- "float t=F[2];"
- "const vec3 E=vec3(0.,.01,1.);"
- "const float PI=3.14159,PI2=PI*2.,rt=.1,zt=.3;\n"
- "#define Z(s)textureSize(S[s],0)\n"
- "#define T(s,c)texture2D(S[s],(c)/Z(s))\n"
- "#define Q(s,c)texture2D(S[s],(c)/Z(1))\n"
- "#define X gl_FragCoord.xy\n";
+"#version 130\n"
+"#define Z(s)textureSize(S[s],0)\n"
+"#define T(s,c)texture2D(S[s],(c)/Z(s))\n"
+"#define X gl_FragCoord.xy\n"
+"uniform sampler2D S[12];"
+"uniform float F[32];"
+//"vec3 f=vec3(F[0],F[1],F[2]),c=vec3(F[3],F[4],F[5]),e=vec3(0.,3.,0.),s=vec3(F[9],F[10],F[11]);"
+"float t=F[0];"
+"const vec3 E=vec3(0.,.01,1.);"
+"const float PI=3.14159,PI2=PI*2.,rt=.1,zt=.3;";
+ 
 
 #endif // HEADER_H_
